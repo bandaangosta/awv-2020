cdf13a14cb03fff86419cbc33b23fbcc6549dde4
Nicolò Parmiggiani <nicolo.parmiggiani@inaf.it>
2020-07-31 11:41:04 -0400

    Scheduler v.0.0.2

--- pyScheduling/src/Scheduler/SchedulerComponentImpl.py
+++ pyScheduling/src/Scheduler/SchedulerComponentImpl.py
@@ -1,3 +1,5 @@
+from threading import Thread, Lock
+
 #Client stubs and definitions, such as structs, enums, etc.
 import SCHEDULER_MODULE
 #Skeleton infrastructure for server implementation
@@ -22,61 +24,105 @@ class SchedulerComponentImpl(SCHEDULER_MODULE__POA.Scheduler, ACSComponent, Cont
         ContainerServices.__init__(self)
         self._logger = self.getLogger()
         self.status=0
-        self._database_comp = None
         self._proposals_list = None
         self.all_proposals_done = False
     
+    def initialize(self):
+        self._database_comp = self.getComponent("DATABASE")
+        self._telescope_comp = self.getComponent("TELESCOPE")
+        self._instrument_comp = self.getComponent("INSTRUMENT")
+    
+    def execute(self):
+        """ (this is a lifecycle method - c.f. lifecycle.idl  I guess :|     )
+            Called after initialize to tell the component that it has to be ready to accept incoming functional calls any time
+            Consider ready to receive calls (Change states if appropriate)
 
+            To me (DN) it is unclear what to put in __init__, initialze or execute at the moment.
+            The C++ people have put the getComponent calls into execute.
+        """
+        pass
+
+    def execute_proposals():
+        """(executed as a target of a thread, which is started in self.start)
+
+        - get list of proposals 
+        - start them
+        - ...
+        """
+        """
+        for proposal in self._database_comp.getProposals():
+            if not proposal:
+                continue
+
+            
+            self._instrument_comp.start_or_so_forgot_name()
+            for target in proposal.targets:
+                image = self._telescope_comp.start(traget)
+
+                self._telescope_comp.storeImage(proposal.id, target.id, image) 
+            self._instrument_comp.stop_or_so_forgot_name()
+
+            
+        """
+        pass
+            
+            
+            
+            
+            # we need a compilable version to push
+            
     def start(self):
-        
+        """(called from the outside)
+
+        start a thread, which is executing, self.execute_proposals (once ... I think)
+        """
         #print("Just printing 'Hello World!'")
-        
-        #try:
 
         if (self.status==1):
-            raise SYSTEMErrImpl.SchedulerAlreadyRunningExImpl().getSchedulerAlreadyRunningEx()
+            raise SYSTEMErrImpl.SchedulerAlreadyRunningExImpl().getSYSTEMErrEx()
        
         self.status=1
         
-        self._database_comp = self.getComponent("DATABASE")
-
-        
         #self._logger.info("log status '%s' and an int (%d)" % ("Log Entry",self._proposals_list[0].status))
          
-
-        while(self.status==1 and not self.all_proposals_done):
-
-            self._proposals_list = self._database_comp.getProposals()
-            
-            self.all_proposals_done=True
-
-            for proposal in self._proposals_list:
-                
-                #proposal.status = 2
-
-                if (proposal.status!=2):
-                    self.all_proposals_done=False
-
-
-                #self._database_comp.setProposalStatus(proposal.pid, 2)
-            
-                self._logger.info("log status '%s' and an int (%d)" % ("Log Entry",proposal.status))
+        self._proposals_list = self._database_comp.getProposals()
         
-                
-                #print(proposal.status . "\n")
-
-                #self._logger.logInfo(proposal)
+        self.all_proposals_done=True
+        
             
+        for proposal in self._proposals_list:
             
+            #proposal.status = 2
 
-        #except SYSTEMErr.AlreadyInAutomaticEx as e:
-         
+            if not proposal:
+                self._logger.info("Empty proposal. Stop")
+                continue
         
 
+            if (proposal.status!=2):
+                self.all_proposals_done=False
 
-    def stop(self):
+            
+            # loop over targets
 
-        if (self.status==0):
-           raise SYSTEMErrImpl.SchedulerAlreadyStoppedExImpl().getSchedulerAlreadStoppedEx()
-     
-        self.status=0
+                # for each target call observe() from Telescope
+                
+    
+
+            #self._database_comp.setProposalStatus(proposal.pid, 2)
+        
+            self._logger.info("log status '%s' and an int (%d)" % ("Log Entry",proposal.status))
+    
+            
+            #print(proposal.status . "\n")
+
+            #self._logger.logInfo(proposal)
+        
+
+
+def stop(self):
+
+    if (self.status==0):
+        raise SYSTEMErrImpl.SchedulerAlreadyStoppedExImpl().getSchedulerAlreadStopp
+
+    self.status=0
